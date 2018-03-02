52fb7ea7e872b1da9717276d3730ebbda8e0b36e
SalilVishnuKapur <salilvishnukapur@gmail.com>
2018-03-02 12:45:52 -0400

    Added the Linear Regression and ANN implementation using Tensorflow

---
+++ ANN_TF.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import numpy as np
+import sklearn
+
+
+# Here we need to first create a input for our network
+
+
+# Then we need to write the equations for as many hidden layers as we require
+
+
+
+# Then we need to need to find the cost function
+
+
+
+
+# After that we calculate the cost
+
+
+
+
+# On the bases of the cost and learning rate we fine tune the weights 
+
+
+
+
+# Then finally we have our trained model
+
+ 
+
+
+
+
+
+
